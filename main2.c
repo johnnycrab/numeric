0816042806d84c0d1bcc53ce7873923fcf40cba4
Jonathan Pirnay <mail@johnnycrab.com>
2015-12-03 19:51:16 +0100

    basic setup

--- main2.c
+++ main2.c
@@ -9,6 +9,18 @@
 #include <stdlib.h>
 #include "LRmP.h"
 
+/**
+ * Teilaufgabe i.)
+ */
+
+double Bisektionsverfahren(double (*f)(double x), double a, double b, int maxiter, double tol, FILE *file) {
+	return NULL;
+}
+
+double Newtonverfahren(double (*f)(double x), double (*df)(double x), double x0, int maxiter, double tol, FILE *file) {
+	return NULL;
+}
+
 int main(void) {
 
 }
